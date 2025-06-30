68ee76826bd68b97d48ed461d9e7dd922ae78ac1
Renan <renansantos.trab@gmail.com>
2025-06-30 14:34:41 +0100

    remove item function

--- scripts.js
+++ scripts.js
@@ -76,8 +76,13 @@ function expenseAdd(newExpense) {
     expenseAmount.classList.add("expense-amount")
     expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "remove item")
+
     //add information to item
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     //add item to the list  
     expenseList.append(expenseItem)
